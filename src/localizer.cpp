52604133f2cb6a98eed19000785b462e8a65bf0f
Toshiki Nakamura <ee193014@meiji.ac.jp>
2022-04-08 19:45:35 +0900

    コメントの削除

--- src/localizer.cpp
+++ src/localizer.cpp
@@ -67,7 +67,6 @@ void ParticleFilter::move(double distance, double direction, double rotation)
     new_yaw = optimize_angle(getPose_yaw() + rotation);
 
     set(new_x, new_y, new_yaw, get_weight());
-    // hello! test2
 }
 //Hello!!!!!
 double set_noise(double mu, double cov)
