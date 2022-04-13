a92ac859ed9153f1f66f9fa83abbddf0aed7e61e
YukiWakabayashi0922 <ee193143@meiji.ac.jp>
2022-04-13 15:15:57 +0900

    できたかも

--- include/local_map_creator/local_map_creator.h
+++ include/local_map_creator/local_map_creator.h
@@ -2,11 +2,9 @@
 #define local_map_cretor_H
 
 #include "ros/ros.h"
-#include "roomba_500driver_meiji/RoombaCtrl.h"
 #include "sensor_msgs/LaserScan.h"
 #include "nav_msgs/OccupancyGrid.h"
 #include "geometry_msgs/PoseArray.h"
-#include "tf/tf.h"
 
 class LocalMapCreator
 {
@@ -15,35 +13,28 @@ class LocalMapCreator
         void process();
     private:
         void laser_callback(const sensor_msgs::LaserScan::ConctPtr&);
-        void odometry_callback(const nav_msgs::Odometry::ConstPtr&);
         void create_line(double yaw, double laser_range);
         void create_local_map();
         void init_map();
         int xy_to_map_index(double x, double y);
-        float get_yaw();
         bool is_map_range_checker(double x, double y);
         bool is_ignore_angle_checker(double angle);
 
-        int hz;
-        int map_size;
-        double map_reso;
-        double laser_density;
-        double roomba_radius;
-        double ignore_angle_mergin;
+        int hz_;
+        double map_size_;
+        double map_reso_;
+        double roomba_radius_;
 
         bool is_laser_checker = false;
-        bool is_odometry_checker = false;
 
         ros::NodeHandle nh;
         ros::NodeHandle private_nh;
 
         ros::Subscriber laser_sub;
-        ros::Subscriber odometry_sub;
         ros::Publisher local_map_pub;
         ros::Publisher obstacle_poses_pub;
 
         sensor_msgs::LaserScan laser;
-        nav_msgs::Odometry odometry;
         nav_msgs::OccupancyGrid local_map;
         geometry_msgs::PoseArray obstacle_poses;
 };
