bff37754fd3ea173c888cc5a573391b92cda4440
YukiWakabayashi0922 <ee193143@meiji.ac.jp>
2022-04-14 18:05:43 +0900

    unfinished

--- src/local_map_creator.cpp
+++ src/local_map_creator.cpp
@@ -39,10 +39,10 @@ void LocalMapCreator::init_map()
 
 int LocalMapCreator::xy_to_map_index(double x, double y)
 {
-    int index_x = int((x - local_map_.info.origin.position.x) / local_map_.info.resolution);
-    int index_y = int((y - local_map_.info.origin.position.y) / local_map_.info.resolution);
+    double index_x = (x - local_map_.info.origin.position.x) / local_map_.info.resolution;
+    double index_y = (y - local_map_.info.origin.position.y) / local_map_.info.resolution;
 
-    return index_x + index_y;
+    return int(index_x + index_y);
 }
 
 bool LocalMapCreator::is_map_range_checker(double x, double y)
@@ -76,22 +76,23 @@ bool LocalMapCreator::is_ignore_angle_checker(double angle)
 
 void LocalMapCreator::create_line(double angle, double laser_range)
 {
-    if(laser_range <= roomba_radius_) {
-            laser_range = map_size_;
-    }
+    // if(laser_range <= roomba_radius_) {
+            // laser_range = map_size_;
+    // }
 
     double x_now = 0;
     double y_now = 0;
-    int map_index = xy_to_map_index(x_now, y_now);
+    int map_index = 0;
 
     for(double dist_from_start = 0; dist_from_start < map_size_; dist_from_start += map_reso_) {
         x_now = dist_from_start * std::cos(angle);
         y_now = dist_from_start * std::sin(angle);
+        map_index = xy_to_map_index(x_now, y_now);
 
-        if(is_map_range_checker(x_now, y_now) == false) {
+        if(!is_map_range_checker(x_now, y_now)) {
             return;
         }
-        if(is_ignore_angle_checker(angle) == true) {
+        if(is_ignore_angle_checker(angle)) {
             if(dist_from_start >= laser_range) {
                 local_map_.data[map_index] = 100;
                 geometry_msgs::Pose obstacle_pose_;
@@ -110,11 +111,10 @@ void LocalMapCreator::create_line(double angle, double laser_range)
 void LocalMapCreator::create_local_map()
 {
     obstacle_poses_.poses.clear();
-    int scan_size = laser_.ranges.size();
-    int scan_step = (laser_.angle_max - laser_.angle_min) / scan_size;
+    int scan_size = (laser_.angle_max - laser_.angle_min) / laser_.angle_increment;
     double angle = 0;
 
-    for(int i=0; i<scan_size; i+=scan_step) {
+    for(int i=0; i<scan_size; i++) {
         angle = i * laser_.angle_increment + laser_.angle_min;
         create_line(angle, laser_.ranges[i]);
     }
@@ -124,7 +124,7 @@ void LocalMapCreator::process()
 {
     ros::Rate loop_rate(hz_);
     while(ros::ok()) {
-        if(is_laser_checker_ = true) {
+        if(is_laser_checker_) {
             init_map();
             create_local_map();
             local_map_pub_.publish(local_map_);
