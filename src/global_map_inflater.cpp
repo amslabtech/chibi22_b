24a0a694906e42a9af189af563470ff463e58ea2
GentaSasa <sasa@DESKTOP-90DKI87.localdomain>
2022-04-19 01:55:22 +0900

    write the code neatly

--- src/global_map_inflater.cpp
+++ src/global_map_inflater.cpp
@@ -6,7 +6,7 @@ Global_Map_Inflater::Global_Map_Inflater():private_nh("~"),nh("")
     private_nh.param("row_",row_,{2000});                                      //マップの行数は適宜設定する
     private_nh.param("column_",column_,{2000});                                //マップの列数は適宜設定する
     update_map = nh.advertise<nav_msgs::OccupancyGrid>("/new_map",1);          //更新したマップをpublish(出版)するノード
-    original_map = nh.subscribe("/map",10,&Global_Map_Inflater::callback,this); //元のマップをsubscribe(購読)するノード
+    original_map = nh.subscribe("/map",10,&Global_Map_Inflater::callback,this);//元のマップをsubscribe(購読)するノード
 }
 
 void Global_Map_Inflater::callback(const nav_msgs::OccupancyGrid::ConstPtr& msg)//callback関数
@@ -23,10 +23,10 @@ void Global_Map_Inflater::callback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
     ROS_INFO("sended new_map");
 }
 
-void Global_Map_Inflater::map2array()                                           //マップ情報を配列へ格納する関数
+void Global_Map_Inflater::map2array()//マップ情報を配列へ格納する関数
 {
     std::cout<<"map2array starts"<<std::endl;
-    if(origin_map.data.size() != 0)                                            //map情報が入っているかの確認
+    if(origin_map.data.size() != 0)//map情報が入っているかの確認
     {
       std::cout<<"origin map input OK"<<std::endl;
     }
@@ -54,7 +54,7 @@ void Global_Map_Inflater::map2array()                                           
     }
 }
 
-void Global_Map_Inflater::obstacle_expand()                                     //マップの障害物を拡大する
+void Global_Map_Inflater::obstacle_expand()//マップの障害物を拡大する
 {
     std::cout<<"Obstacle expand starts"<<std::endl;
 
@@ -76,7 +76,7 @@ void Global_Map_Inflater::obstacle_expand()                                     
     }
 }
 
-void Global_Map_Inflater::remap()                                               //配列からメッセージとして送信する関数
+void Global_Map_Inflater::remap()//配列からメッセージとして送信する関数
 {
     std::cout<<"remap starts"<<std::endl;
     new_map.header = origin_map.header;
@@ -91,7 +91,7 @@ void Global_Map_Inflater::remap()                                               
         }
     }
 
-    if(new_map.data.size() != 0)                                               //map情報が入っているかの確認
+    if(new_map.data.size() != 0)//map情報が入っているかの確認
     {
       std::cout<<"new map export OK"<<std::endl;
     }
@@ -102,9 +102,9 @@ void Global_Map_Inflater::remap()                                               
 
 }
 
-void Global_Map_Inflater::process()                                             //プロセス関数
+void Global_Map_Inflater::process()//プロセス関数
 {
-    grid_map = std::vector<std::vector<int>>(row_, std::vector<int>(column_,0)); //配列の初期化
+    grid_map = std::vector<std::vector<int>>(row_, std::vector<int>(column_,0));//配列の初期化
     copy_grid_map = std::vector<std::vector<int>>(row_, std::vector<int>(column_,0));
     std::cout<<"process starts"<<std::endl;
     ros::Rate loop_rate(hz_);
