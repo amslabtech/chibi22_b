e0c9e5c754d74d5d11ba3644fc9bc304b1db9800
YukiWakabayashi0922 <ee193143@meiji.ac.jp>
2022-05-08 13:59:31 +0900

    補間のソースファイルを追加

--- include/local_map_creator/local_map_creator_test.h
+++ include/local_map_creator/local_map_creator_test.h
@@ -2,9 +2,12 @@
 #define local_map_creator_H
 
 #include "ros/ros.h"
+#include "tf2/utils.h"
 #include "sensor_msgs/LaserScan.h"
 #include "nav_msgs/OccupancyGrid.h"
+#include "nav_msgs/Odometry.h"
 #include "geometry_msgs/PoseArray.h"
+#include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Pose.h"
 
 class LocalMapCreator
@@ -14,7 +17,8 @@ class LocalMapCreator
         void process();
     private:
         void laser_callback(const sensor_msgs::LaserScan::ConstPtr &msg);
-        void create_line(double angle, double laser_range);
+        void pose_callback(const geometry_msgs::PoseStamped::ConstPtr &msg);
+        void create_line(double angle, double laser_range, bool first_map_checker_);
         void create_local_map();
         void init_map();
         int xy_to_map_index(double x, double y);
@@ -26,21 +30,32 @@ class LocalMapCreator
         double map_size_;
         double map_reso_;
         double roomba_radius_;
+        bool flag_map_view_;
 
         bool is_laser_checker_ = false;
-        bool flag_map_view = true;
+        bool is_first_pose_checker_ = false;
+        bool is_second_pose_checker_ = false;
+        bool first_map_checker_ = false;
 
         ros::NodeHandle nh_;
         ros::NodeHandle private_nh_;
 
         ros::Subscriber laser_sub_;
+        ros::Subscriber pose_sub_;
         ros::Publisher local_map_pub_;
         ros::Publisher obstacle_poses_pub_;
 
         sensor_msgs::LaserScan laser_;
+        geometry_msgs::PoseStamped current_pose_;
+        nav_msgs::Odometry current_odo_;
+        geometry_msgs::Pose previous_pose_;
+        nav_msgs::Odometry previous_odo_;
+        geometry_msgs::Pose diff_;
         nav_msgs::OccupancyGrid local_map_;
         geometry_msgs::PoseArray obstacle_poses_;
+        geometry_msgs::PoseArray blind_obstacle_poses_;
         geometry_msgs::Pose obstacle_pose_;
+        geometry_msgs::Pose blind_obstacle_pose_;
 };
 
 #endif
